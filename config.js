dda9c47f256c7b6b528ad095379160cd2b988d6b
Benjamin Sproule <benjamin@benjaminsproule.com>
2023-05-25 08:45:29 +0100

    Added custom env variables

--- config.js
+++ config.js
@@ -1,9 +1,12 @@
 module.exports = {
-    hostRules: [
-        {
-            hostType: 'npm',
-            matchHost: 'registry.npmjs.org',
-            token: process.env.NPM_AUTH_READ_TOKEN,
-        },
-    ],
-}
+  hostRules: [
+    {
+      hostType: "npm",
+      matchHost: "registry.npmjs.org",
+      token: process.env.NPM_AUTH_READ_TOKEN,
+    },
+  ],
+  customEnvVariables: {
+    NPM_AUTH_READ_TOKEN: process.env.NPM_AUTH_READ_TOKEN,
+  },
+};
