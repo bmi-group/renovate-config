3357c2c91a58c6c6f04c62e26746c01d720bef0a
Benjamin Sproule <benjamin@benjaminsproule.com>
2023-05-25 18:03:39 +0100

    Updated README

--- config.js
+++ config.js
@@ -8,7 +8,6 @@ module.exports = {
   ],
   npmrc: process.env.RENOVATE_NPMRC,
   customEnvVariables: {
-    GITHUB_COM_TOKEN: process.env.GITHUB_COM_TOKEN,
     NPM_AUTH_READ_TOKEN: process.env.NPM_AUTH_READ_TOKEN,
     RENOVATE_NPMRC: process.env.RENOVATE_NPMRC,
   },
