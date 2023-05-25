1b1559ef64f8007d0d1bacfc9bcc3c198fdef987
Benjamin Sproule <benjamin@benjaminsproule.com>
2023-05-25 18:21:53 +0100

    Remove npmrc config

--- config.js
+++ config.js
@@ -6,7 +6,6 @@ module.exports = {
       token: process.env.NPM_AUTH_READ_TOKEN,
     },
   ],
-  npmrc: process.env.RENOVATE_NPMRC,
   customEnvVariables: {
     NPM_AUTH_READ_TOKEN: process.env.NPM_AUTH_READ_TOKEN,
   },
