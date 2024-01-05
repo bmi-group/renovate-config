d9371a8ebd29cf512f3ad4132e31997d8e37ac53
Benjamin Sproule <benjamin.sproule@bmigroup.com>
2024-01-05 10:42:59 +0000

    Remove unnecessary config

--- config.js
+++ config.js
@@ -13,44 +13,5 @@ module.exports = {
     },
   ],
   labels: ["dependencies"],
-  lockFileMaintenance: {
-    enabled: true,
-    automerge: true,
-    automergeType: "pr",
-    platformAutomerge: true,
-    autoApprove: true,
-  },
   onboarding: true,
-  packageRules: [
-    {
-      matchPackageNames: ["@bmi-digital/components"],
-      automerge: true,
-      automergeType: "pr",
-      platformAutomerge: true,
-      autoApprove: true,
-    },
-    {
-      matchDepPatterns: ["^@mui"],
-      automerge: true,
-      automergeType: "pr",
-      platformAutomerge: true,
-      autoApprove: true,
-    },
-    {
-      excludeDepNames: ["@bmi-digital/components"],
-      excludeDepPatterns: ["^@mui"],
-      matchUpdateTypes: ["minor", "patch"],
-      matchCurrentVersion: "!/^0/",
-      automerge: true,
-      automergeType: "pr",
-      platformAutomerge: true,
-      groupName: "all non-major dependencies",
-      groupSlug: "all-minor-patch",
-      autoApprove: true,
-    },
-    {
-      matchPackageNames: ["@lhci/cli"],
-      allowedVersions: "!/^0.13.0$/"
-    }
-  ],
 };
